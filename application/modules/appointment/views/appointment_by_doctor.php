eb807b5d53ad25d01bf25c18131bd942595352a3
lotusCupcake <fikriansari.mfa@gmail.com>
2023-05-25 17:33:22 +0700

    update: view

--- application/modules/appointment/views/appointment_by_doctor.php
+++ application/modules/appointment/views/appointment_by_doctor.php
@@ -112,10 +112,6 @@
     <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title"><?php echo lang('choose_payment_type'); ?></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
             </div>
             <div class="modal-body" id='medical_history'>
             </div>
