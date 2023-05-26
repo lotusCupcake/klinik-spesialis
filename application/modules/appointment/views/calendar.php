ebb8b5c452ed1969a4ed9d74ec3a636fbfaee175
Muhammad Arizky <arizkymuhammad46@gmail.com>
2023-05-26 12:17:50 +0700

    fix calendar view

--- application/modules/appointment/views/calendar.php
+++ application/modules/appointment/views/calendar.php
@@ -1,14 +1,11 @@
-
 <!--sidebar end-->
 <!--main content start-->
-<section id="main-content">
-    <section class="wrapper site-min-height">
-        <!-- page start-->
-        <section class="panel">
-            <header class="panel-heading">
-                <?php echo lang('appointment'); ?> <?php echo lang('calendar'); ?>
-            </header>
-            <div class="panel-body">
+<div class="main-content">
+    <section class="section">
+        <div class="section-header">
+            <h1><?php echo lang('appointment'); ?> <?php echo lang('calendar'); ?></h1>
+        </div>
+        <div class="section-body">
                 <aside>
                     <section class="panel">
                         <div class="panel-body">
@@ -16,34 +13,29 @@
                         </div>
                     </section>
                 </aside>
-            </div>
-        </section>
-        <!-- page end-->
+        </div>
     </section>
-</section>
-<!--main content end-->
-<!--footer start-->
-<div class="modal fade" tabindex="-1" role="dialog" id="cmodal">
-    <div class="modal-dialog modal-lg" role="document" style="width: 80%;">
+</div>
+
+<div class="modal fade" role="dialog" id="cmodal">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
-            <!--
             <div class="modal-header">
-                <h5 class="modal-title"><?php echo lang('patient') . " " . lang('history'); ?></h5>
+            
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            -->
-            <div id='medical_history'>
-                <div class="col-md-12">
+            <div class="modal-body">
+                <div id='medical_history'>
+                    <div class="col-md-12">
 
-                </div> 
-            </div>
-            <div class="modal-footer">
-                <div class="col-md-12">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div> 
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<!--main content end-->
+<!--footer start-->
