8911da24786fcf217ab45fc928af109fdcf4ae2d
Muhammad Arizky <arizkymuhammad46@gmail.com>
2023-06-08 11:34:59 +0700

    fix: add new and medicine category view

--- application/modules/medicine/views/add_new_category_view.php
+++ application/modules/medicine/views/add_new_category_view.php
@@ -1,53 +1,82 @@
 <!--sidebar end--> 
 <!--main content start-->
-<section id="main-content">
-    <section class="wrapper site-min-height">
-        <!-- page start-->
-        <section class="col-md-6 row">
-            <header class="panel-heading">
+<div class="main-content">
+    <div class="section">
+        <div class="section-header">
+            <h1>
                 <?php
                 if (!empty($medicine->id))
                     echo lang('edit_medicine_category');
                 else
                     echo lang('add_medicine_category');
                 ?>
-            </header>
-            <div class="">
-                <div class="adv-table editable-table ">
-                    <div class="clearfix">
-                        <div class="panel-body">
-                                    <?php echo validation_errors(); ?>
-                                    <form role="form" action="medicine/addNewCategory" class="clearfix" method="post" enctype="multipart/form-data">
-                                        <div class="form-group"> 
-                                            <label for="exampleInputEmail1"> <?php  echo lang('category'); ?> <?php  echo lang('name'); ?> </label>
-                                            <input type="text" class="form-control" name="category" id="exampleInputEmail1" value='<?php
+            </h1>
+        </div>
+        <?php
+        $message = validation_errors();
+        if (!empty($message)) {
+        ?><div class="alert alert-danger alert-has-icon alert-dismissible show fade">
+                <div class="alert-icon"><i class="fas fa-exclamation-triangle"></i></div>
+                <div class="alert-body">
+                    <button class="close" data-dismiss="alert">
+                        <span>&times;</span>
+                    </button>
+                    <div class="alert-title">Failed!</div>
+                    <?= $message; ?>
+                </div>
+            </div>
+        <?php } ?>
+        <div class="section-body">
+            <div class="card">
+                <div class="card-body">
+                    <form role="form" action="medicine/addNewCategory" class="clearfix" method="post" enctype="multipart/form-data">
+                        <div class="row" style="padding-right:30px">
+                            <div class="col-md-12 row mb-4">
+                                <div class="col-md-4 text-right">
+                                    <label class="col-form-label"><?php  echo lang('category'); ?> <?php  echo lang('name'); ?></label>
+                                </div>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control" name="category" id="exampleInputEmail1" value='<?php
                                             if (!empty($medicine->category)) {
                                                 echo $medicine->category;
                                             }
-                                            ?>' placeholder="">    
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="exampleInputEmail1"> <?php  echo lang('description'); ?></label>
-                                            <input type="text" class="form-control" name="description" id="exampleInputEmail1" value='<?php
+                                            ?>' placeholder=""> 
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row" style="padding-right:30px">
+                            <div class="col-md-12 row mb-4">
+                                <div class="col-md-4 text-right">
+                                    <label class="col-form-label"><?php  echo lang('description'); ?></label>
+                                </div>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control" name="description" id="exampleInputEmail1" value='<?php
                                             if (!empty($medicine->description)) {
                                                 echo $medicine->description;
                                             }
-                                            ?>' placeholder="">
-                                        </div>
-                                        <input type="hidden" name="id" value='<?php
-                                        if (!empty($medicine->id)) {
-                                            echo $medicine->id;
-                                        }
-                                        ?>'>
-                                        <button type="submit" name="submit" class="btn btn-info"> <?php  echo lang('submit'); ?></button>
-                                    </form>
+                                            ?>' placeholder=""> 
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                        <input type="hidden" name="id" value='<?php
+                            if (!empty($medicine->id)) {
+                                echo $medicine->id;
+                            }
+                        ?>'>
+                        <div class="row" style="padding-right:30px">
+                            <div class="col-md-12 row mb-4">
+                                <div class="col-md-4 text-right">
+                                </div>
+                                <div class="col-md-8">
+                                    <button type="submit" name="submit" class="btn btn-primary"><?php echo lang('submit'); ?></button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-        </section>
-        <!-- page end-->
-    </section>
-</section>
+        </div>
+    </div>
+</div>
 <!--main content end-->
 <!--footer start-->
