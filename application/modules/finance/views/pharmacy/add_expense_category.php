35bed9730decd1613b4cfb824aef09c72e5ca8b1
lotusCupcake <fikriansari.mfa@gmail.com>
2023-06-10 14:38:02 +0700

    update: view finance/pharmacy/expense, finance/pharmacy/add_expense_category, finance/pharmacy/expense_category, finance/pharmacy/add_expense_view,,

--- application/modules/finance/views/pharmacy/add_expense_category.php
+++ application/modules/finance/views/pharmacy/add_expense_category.php
@@ -1,55 +1,77 @@
-<!--sidebar end-->
-<!--main content start-->
-<section id="main-content">
-    <section class="wrapper site-min-height">
-        <!-- page start-->
-        <section class="col-md-6">
-            <header class="panel-heading">
-                <?php
+<!-- Main Content -->
+<div class="main-content no-print">
+    <section class="section">
+        <div class="section-header">
+            <h1><?php
                 if (!empty($category->id))
                     echo lang('pharmacy') . ' ' . lang('edit_expense_category');
                 else
                     echo lang('pharmacy') . ' ' . lang('add_expense_category');
                 ?>
-            </header>
-            <div class="panel-body">
-                <div class="adv-table editable-table ">
-                    <div class="clearfix">
-                        <?php echo validation_errors(); ?>
-                        <form role="form" action="finance/pharmacy/addExpenseCategory" method="post" enctype="multipart/form-data">
-                            <div class="form-group"> 
-                                <label for="exampleInputEmail1"> <?php echo lang('category'); ?> </label>
-                                <input type="text" class="form-control" name="category" id="exampleInputEmail1" value='<?php
-                                if (!empty($category->category)) {
-                                    echo $category->category;
-                                }
-                                ?>' placeholder="">    
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputEmail1"> <?php echo lang('description'); ?> </label>
-                                <input type="text" class="form-control" name="description" id="exampleInputEmail1" value='<?php
-                                if (!empty($category->description)) {
-                                    echo $category->description;
-                                }
-                                ?>' placeholder="">
-                            </div>
-                            <input type="hidden" name="id" value='<?php
-                            if (!empty($category->id)) {
-                                echo $category->id;
-                            }
-                            ?>'>
-                            
-                            
-                                <button type="submit" name="submit" class="btn btn-info pull-right"> <?php echo lang('submit'); ?> </button>
-                            
-                                
-                        </form>
-                    </div>
+            </h1>
+        </div>
+        <?php
+        $message = validation_errors();
+        if (!empty($message)) {
+        ?><div class="alert alert-danger alert-has-icon alert-dismissible show fade">
+                <div class="alert-icon"><i class="fas fa-exclamation-triangle"></i></div>
+                <div class="alert-body">
+                    <button class="close" data-dismiss="alert">
+                        <span>&times;</span>
+                    </button>
+                    <div class="alert-title">Failed!</div>
+                    <?= $message; ?>
                 </div>
             </div>
-        </section>
-        <!-- page end-->
+        <?php } ?>
+        <div class="section-body">
+            <div class="card">
+                <div class="card-body">
+                    <form role="form" action="finance/pharmacy/addExpenseCategory" method="post" enctype="multipart/form-data">
+                        <div class="row" style="padding-right:30px">
+                            <div class="col-md-12 row mb-4">
+                                <div class="col-md-4 text-right">
+                                    <label class="col-form-label"><?php echo lang('category'); ?></label>
+                                </div>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control" name="category" id="exampleInputEmail1" value='<?php
+                                                                                                                            if (!empty($category->category)) {
+                                                                                                                                echo $category->category;
+                                                                                                                            }
+                                                                                                                            ?>' placeholder="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row" style="padding-right:30px">
+                            <div class="col-md-12 row mb-4">
+                                <div class="col-md-4 text-right">
+                                    <label class="col-form-label"><?php echo lang('description'); ?></label>
+                                </div>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control" name="description" id="exampleInputEmail1" value='<?php
+                                                                                                                                if (!empty($category->description)) {
+                                                                                                                                    echo $category->description;
+                                                                                                                                }
+                                                                                                                                ?>' placeholder="">
+                                </div>
+                            </div>
+                        </div>
+                        <input type="hidden" name="id" value='<?php
+                                                                if (!empty($category->id)) {
+                                                                    echo $category->id;
+                                                                }
+                                                                ?>'>
+                        <div class="row" style="padding-right:30px">
+                            <div class="col-md-12 row mb-4">
+                                <div class="col-md-4 text-right">
+                                </div>
+                                <div class="col-md-8">
+                                    <button type="submit" name="submit" class="btn btn-primary"><?php echo lang('submit'); ?></button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
     </section>
-</section>
-<!--main content end-->
-<!--footer start-->
+</div>
--- application/modules/finance/views/pharmacy/expense_category.php
+++ application/modules/finance/views/pharmacy/expense_category.php
@@ -1,109 +1,111 @@
-<!--sidebar end-->
-<!--main content start-->
-<section id="main-content">
-    <section class="wrapper site-min-height">
-        <!-- page start-->
-        <section class="panel">
-            <header class="panel-heading">
-                <?php echo lang('pharmacy'); ?> <?php echo lang('expense_categories'); ?>  
-                <div class="col-md-4 no-print pull-right"> 
-                    <a href="finance/pharmacy/addExpenseCategoryView">
-                        <div class="btn-group pull-right">
-                            <button id="" class="btn green btn-xs">
-                                <i class="fa fa-plus-circle"></i> <?php echo lang('add_expense_category'); ?>
-                            </button>
-                        </div>
-                    </a>
+<!-- Main Content -->
+<div class="main-content">
+    <section class="section">
+        <div class="section-header">
+            <h1><?php echo lang('pharmacy'); ?> <?php echo lang('expense_categories'); ?></h1>
+        </div>
+        <div class="section-body">
+            <div class="card">
+                <div class="card-header">
+                    <a href="finance/pharmacy/addExpenseCategoryView"><button class="btn btn-icon icon-left btn-primary"><i class="fas fa-plus"></i> <?php echo lang('add_expense_category'); ?></button></a>
                 </div>
-            </header>
-            <div class="panel-body">
-                <div class="adv-table editable-table ">
-                    <div class="space15"></div>
-                    <table class="table table-striped table-hover table-bordered" id="editable-sample">
-                        <thead>
-                            <tr>
-                                <th> <?php echo lang('category'); ?> </th>
-                                <th> <?php echo lang('description'); ?> </th>
-                                <?php if ($this->ion_auth->in_group('admin')) { ?>
-                                    <th> <?php echo lang('options'); ?> </th>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <div class="space15"></div>
+                        <table class="table table-striped table-bordered" id="editable-sample">
+                            <thead>
+                                <tr>
+                                    <th><?php echo lang('category'); ?> </th>
+                                    <th><?php echo lang('description'); ?> </th>
+                                    <?php if ($this->ion_auth->in_group('admin')) { ?>
+                                        <th><?php echo lang('options'); ?> </th>
+                                    <?php } ?>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($categories as $category) { ?>
+                                    <tr class="">
+                                        <td><?php echo $category->category; ?></td>
+                                        <td> <?php echo $category->description; ?></td>
+                                        <?php if ($this->ion_auth->in_group('admin')) { ?>
+                                            <td>
+                                                <a href="finance/pharmacy/editExpenseCategory?id=<?php echo $category->id; ?>"><button class="btn btn-icon icon-left btn-light editbutton"><i class="fas fa-edit"></i><?php echo lang('edit'); ?></button></a>
+                                                <a href="finance/pharmacy/deleteExpenseCategory?id=<?php echo $category->id; ?>"><button class="btn btn-icon icon-left btn-danger delete_button" onclick="return confirm('Are you sure you want to delete this item?');"><i class="fas fa-trash"></i><?php echo lang('delete'); ?></button></a>
+                                            </td>
+                                        <?php } ?>
+                                    </tr>
                                 <?php } ?>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <style>
-
-                            .img_url{
-                                height:20px;
-                                width:20px;
-                                background-size: contain; 
-                                max-height:20px;
-                                border-radius: 100px;
-                            }
-
-                        </style>
-
-                        <?php foreach ($categories as $category) { ?>
-                            <tr class="">
-                                <td><?php echo $category->category; ?></td>
-                                <td> <?php echo $category->description; ?></td>
-                                <?php if ($this->ion_auth->in_group('admin')) { ?>
-                                    <td>
-                                        <a class="btn btn-info btn-xs editbutton width_auto" href="finance/pharmacy/editExpenseCategory?id=<?php echo $category->id; ?>"><i class="fa fa-edit"></i>  <?php echo lang('edit'); ?></a>
-                                        <a class="btn btn-info btn-xs delete_button width_auto" href="finance/pharmacy/deleteExpenseCategory?id=<?php echo $category->id; ?>" onclick="return confirm('Are you sure you want to delete this item?');"><i class="fa fa-trash"></i>  <?php echo lang('delete'); ?></a>
-                                    </td>
-                                <?php } ?>
-                            </tr>
-                        <?php } ?>
-
-
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </section>
-        <!-- page end-->
+        </div>
     </section>
-</section>
-<!--main content end-->
-<!--footer start-->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</div>
+
+<script src="common/js/codearistos.min.js"></script>
 <script>
-                                    $(document).ready(function () {
-                                        var table = $('#editable-sample').DataTable({
-                                            responsive: true,
+    $(document).ready(function() {
+        var table = $('#editable-sample').DataTable({
+            responsive: true,
+            dom: "<'row'<'col-sm-3'l><'col-sm-5 text-center'B><'col-sm-4'f>>" +
+                "<'row'<'col-sm-12'tr>>" +
+                "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+            buttons: [{
+                    extend: 'copyHtml5',
+                    exportOptions: {
+                        columns: [0, 1, 2],
+                    }
+                },
+                {
+                    extend: 'excelHtml5',
+                    exportOptions: {
+                        columns: [0, 1, 2],
+                    }
+                },
+                {
+                    extend: 'csvHtml5',
+                    exportOptions: {
+                        columns: [0, 1, 2],
+                    }
+                },
+                {
+                    extend: 'pdfHtml5',
+                    exportOptions: {
+                        columns: [0, 1, 2],
+                    }
+                },
+                {
+                    extend: 'print',
+                    exportOptions: {
+                        columns: [0, 1, 2],
+                    }
+                },
+            ],
+            aLengthMenu: [
+                [10, 25, 50, 100, -1],
+                [10, 25, 50, 100, "All"]
+            ],
+            iDisplayLength: -1,
+            "order": [
+                [0, "desc"]
+            ],
 
-                                            dom: "<'row'<'col-sm-3'l><'col-sm-5 text-center'B><'col-sm-4'f>>" +
-                                                    "<'row'<'col-sm-12'tr>>" +
-                                                    "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+            "language": {
+                "lengthMenu": "_MENU_",
+                search: "_INPUT_",
+                "url": "common/assets/DataTables/languages/<?php echo $this->language; ?>.json"
+            },
 
-                                            buttons: [
-                                                {extend: 'copyHtml5', exportOptions: {columns: [0, 1], }},
-                                                {extend: 'excelHtml5', exportOptions: {columns: [0, 1], }},
-                                                {extend: 'csvHtml5', exportOptions: {columns: [0, 1], }},
-                                                {extend: 'pdfHtml5', exportOptions: {columns: [0, 1], }},
-                                                {extend: 'print', exportOptions: {columns: [0, 1], }},
-                                            ],
-                                            aLengthMenu: [
-                                                [10, 25, 50, 100, -1],
-                                                [10, 25, 50, 100, "All"]
-                                            ],
-                                            iDisplayLength: -1,
-                                            "order": [[0, "desc"]],
+        });
 
-                                            "language": {
-                                                "lengthMenu": "_MENU_",
-                                                search: "_INPUT_",
-                                                "url": "common/assets/DataTables/languages/<?php echo $this->language; ?>.json"
-                                            },
-
-                                        });
-
-                                        table.buttons().container()
-                                                .appendTo('.custom_buttons');
-                                    });
+        table.buttons().container()
+            .appendTo('.custom_buttons');
+    });
 </script>
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
         $(".flashmessage").delay(3000).fadeOut(100);
     });
 </script>
