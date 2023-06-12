b6f1e0a5299abef1cf1cdb9008c44e2a7cc721ee
Muhammad Arizky <arizkymuhammad46@gmail.com>
2023-06-12 14:15:03 +0700

    fix: sms gateway view

--- application/modules/sms/views/sgateway.php
+++ application/modules/sms/views/sgateway.php
@@ -1,91 +1,103 @@
 <!--sidebar end-->
 <!--main content start-->
-<section id="main-content">
-    <section class="wrapper site-min-height">
-        <!-- page start-->
-        <section class="col-md-7 row"> 
-            <header class="panel-heading">
-                <?php echo lang('sms_gateways'); ?>
-            </header>
-            <div class="panel-body">
-                <div class="adv-table editable-table ">
-                    <div class=" panel clearfix"> 
-                        <button class="export" onclick="javascript:window.print();">Print</button>  
-                    </div>
-                    <div class="space15"></div>
-                    <table class="table table-striped table-hover table-bordered" id="editable-sample">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th><?php echo lang('name'); ?></th>
-                                <th><?php echo lang('website'); ?></th>
-                                <th><?php echo lang('options'); ?></th>
 
-                            </tr>
-                        </thead>
-                        <tbody> 
-                            <?php
-                            $i = 0;
-                            foreach ($sgateways as $sgateway) {
-                                $i = $i + 1;
-                                ?>
-                                <tr class="">
-                                    <td><?php echo $i; ?></td>
-                                    <td><?php
-                                        if (!empty($sgateway->name)) {
-                                            echo $sgateway->name;
-                                        }
-                                        ?>
-                                    </td>
-                                    <td>
-                                        <a class="" href="<?php echo $sgateway->link; ?>" target="_blank">  <i class="fa fa-"> </i> <?php echo $sgateway->link; ?></a> 
-                                    </td>
-                                    <td>
-                                        <a class="btn btn-info btn-xs btn_width" href="sms/settings?id=<?php echo $sgateway->id; ?>">  <i class="fa fa-"> </i> <?php echo lang('manage'); ?></a>                               
-                                    </td>
-
-                                </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
+<div class="main-content">
+    <div class="section">
+        <div class="section-header">
+            <h1><?php echo lang('sms_gateways'); ?></h1>
+        </div>
+        <?php
+        $message = $this->session->flashdata('feedback');
+        if (!empty($message)) {
+        ?><div class="alert alert-primary alert-has-icon alert-dismissible show fade">
+                <div class="alert-icon"><i class="far fa-lightbulb"></i></div>
+                <div class="alert-body">
+                    <button class="close" data-dismiss="alert">
+                        <span>&times;</span>
+                    </button>
+                    <div class="alert-title">Info!</div>
+                    <?= $message ?>
                 </div>
             </div>
-        </section>
-
-
-        <section class="col-md-5"> 
-            <header class="panel-heading">
-                <?php echo lang('select'); ?> <?php echo lang('sms_gateway'); ?> 
-            </header>
-            <div class="panel-body">
-                <form role="form" id="editAppointmentForm" action="settings/selectSmsGateway" class="clearfix" method="post" enctype="multipart/form-data">
-
-
-                    <?php foreach ($sgateways as $sgateway) { ?>
-                        <div class="form-group">
-                            <input type="radio" class=""  readonly="" name="sms_gateway" id="exampleInputEmail1" value='<?php echo $sgateway->name; ?>' placeholder="" <?php
-                            if (!empty($sgateway->name)) {
-                                if ($settings->sms_gateway == $sgateway->name) {
-                                    echo 'checked';
-                                }
-                            }
-                            ?>> <?php echo $sgateway->name; ?>
+        <?php } ?>
+        <div class="section-body">
+            <div class="row">
+                <div class="col-md-7">
+                    <h2 class="section-title"><?php echo lang('sms_gateways'); ?></h2>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <div class="space15"></div>
+                                <table class="table table-striped table-bordered" id="editable-sample">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th><?php echo lang('name'); ?></th>
+                                            <th><?php echo lang('website'); ?></th>
+                                            <th><?php echo lang('options'); ?></th>
+        
+                                        </tr>
+                                    </thead>
+                                    <tbody> 
+                                        <?php
+                                        $i = 0;
+                                        foreach ($sgateways as $sgateway) {
+                                            $i = $i + 1;
+                                            ?>
+                                            <tr class="">
+                                                <td><?php echo $i; ?></td>
+                                                <td><?php
+                                                    if (!empty($sgateway->name)) {
+                                                        echo $sgateway->name;
+                                                    }
+                                                    ?>
+                                                </td>
+                                                <td>
+                                                    <a class="" href="<?php echo $sgateway->link; ?>" target="_blank">  <i class="fa fa-"> </i> <?php echo $sgateway->link; ?></a> 
+                                                </td>
+                                                <td>
+                                                    <a class="btn btn-info btn-xs btn_width" href="sms/settings?id=<?php echo $sgateway->id; ?>">  <i class="fa fa-"> </i> <?php echo lang('manage'); ?></a>                               
+                                                </td>
+        
+                                            </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                    <?php } ?>
-
-
-                    <input type="hidden" name="id" value="<?php echo $settings->id; ?>">
-
-                    <div class="col-md-12 panel">
-                        <button type="submit" name="submit" class="btn btn-info pull-right"> <?php echo lang('submit'); ?></button>
                     </div>
-                </form>
+                </div>
+                <div class="col-md-5">
+                    <h2 class="section-title"><?php echo lang('select'); ?> <?php echo lang('sms_gateway'); ?></h2>
+                    <div class="card">
+                        <div class="card-body">
+                            <form role="form" id="editAppointmentForm" action="settings/selectSmsGateway" class="clearfix" method="post" enctype="multipart/form-data">
+                                <?php foreach ($sgateways as $sgateway) { ?>
+                                    <div class="form-group">
+                                        <input type="radio" class=""  readonly="" name="sms_gateway" id="exampleInputEmail1" value='<?php echo $sgateway->name; ?>' placeholder="" <?php
+                                        if (!empty($sgateway->name)) {
+                                            if ($settings->sms_gateway == $sgateway->name) {
+                                                echo 'checked';
+                                            }
+                                        }
+                                        ?>> <?php echo $sgateway->name; ?>
+                                    </div>
+                                <?php } ?>
+        
+        
+                                <input type="hidden" name="id" value="<?php echo $settings->id; ?>">
+        
+                                <div class="card-footer">
+                                    <button type="submit" name="submit" class="btn btn-primary submit_button float-right mb-4"><?php echo lang('submit'); ?></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </section>
-
-        <!-- page end-->
-    </section>
-</section>
+        </div>
+    </div>
+</div>
 <!--main content end-->
 
 
