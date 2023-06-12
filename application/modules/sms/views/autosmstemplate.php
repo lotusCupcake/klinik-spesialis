e99e181dd376a16b0d6b000949590bcda47eb238
Muhammad Arizky <arizkymuhammad46@gmail.com>
2023-06-12 14:15:28 +0700

    fix: auto sms template view

--- application/modules/sms/views/autosmstemplate.php
+++ application/modules/sms/views/autosmstemplate.php
@@ -1,55 +1,66 @@
-
 <!--sidebar end-->
 <!--main content start-->
-<section id="main-content">
-    <section class="wrapper site-min-height">
-        <!-- page start-->
-        <section class="panel">
-
-            <header class="panel-heading">
-                <?php echo lang('autosmstemplate'); ?>
-            </header>
-
-            <div class="panel-body">
-                <div class="adv-table editable-table ">
-
-                    <div class="space15"></div>
-                    <table class="table table-striped table-hover table-bordered" id="editable-sample1">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th><?php echo lang('category'); ?></th>
-                                <th><?php echo lang('message'); ?></th> 
-                                <th><?php echo lang('status'); ?></th>
-                                <th><?php echo lang('options'); ?></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                        </tbody>
-                    </table>
+<div class="main-content">
+    <div class="section">
+        <div class="section-header">
+            <h1><?php echo lang('autosmstemplate'); ?></h1>
+        </div>
+        <?php
+        $message = $this->session->flashdata('feedback');
+        if (!empty($message)) {
+        ?><div class="alert alert-primary alert-has-icon alert-dismissible show fade">
+                <div class="alert-icon"><i class="far fa-lightbulb"></i></div>
+                <div class="alert-body">
+                    <button class="close" data-dismiss="alert">
+                        <span>&times;</span>
+                    </button>
+                    <div class="alert-title">Info!</div>
+                    <?= $message ?>
                 </div>
             </div>
-        </section>
-        <!-- page end-->
-    </section>
-</section>
+        <?php } ?>
+        <div class="section-body">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <div class="space15"></div>
+                        <table class="table table-striped table-bordered" id="editable-sample1">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th><?php echo lang('category'); ?></th>
+                                    <th><?php echo lang('message'); ?></th> 
+                                    <th><?php echo lang('status'); ?></th>
+                                    <th><?php echo lang('options'); ?></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <!--main content end-->
 
 
 
 <!-- Edit sms temp Modal-->
-<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-    <div class="modal-dialog">
+<div class="modal fade" role="dialog" id="myModal1">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h4 class="modal-title"><?php echo lang('edit'); ?> <?php echo lang('auto'); ?> <?php echo lang('template'); ?></h4>
+                <h5 class="modal-title"><?php echo lang('edit'); ?> <?php echo lang('auto'); ?> <?php echo lang('template'); ?></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-            <div class="modal-body">
+            <form role="form" action="sms/addNewAutoSMSTemplate" name="myform" id="smstemp" method="post" enctype="multipart/form-data">
+                <div class="modal-body">
                 <?php echo validation_errors(); ?>
-                <form role="form" id="smstemp" name="myform" action="sms/addNewAutoSMSTemplate" method="post" enctype="multipart/form-data">                                                                                    
-
                     <div class="form-group">
                         <label for="exampleInputEmail1"> <?php echo lang('category'); ?></label>
                         <input type="text" class="form-control" name="category" id="exampleInputEmail1" value='' placeholder="">
@@ -68,11 +79,14 @@
                     </div>
                     <input type="hidden" name="id" value=''>
                     <input type="hidden" name="type" value='sms'>
-                    <button type="submit" name="submit" class="btn btn-info"><?php echo lang('submit'); ?></button>
-                </form>
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
+                    <div class="modal-footer bg-whitesmoke br">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary"><?php echo lang('submit'); ?></button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 <script src="common/js/codearistos.min.js"></script>
 
