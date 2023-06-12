e02449f8b7da21c9e3448aeb4fe0260c74a850a0
Muhammad Arizky <arizkymuhammad46@gmail.com>
2023-06-12 14:15:48 +0700

    fix: manual sms template view

--- application/modules/sms/views/manual_sms_template.php
+++ application/modules/sms/views/manual_sms_template.php
@@ -1,60 +1,66 @@
 
 <!--sidebar end-->
 <!--main content start-->
-<section id="main-content">
-    <section class="wrapper site-min-height">
-        <!-- page start-->
-        <section class="panel">
-            <header class="panel-heading">
-                <?php echo lang('smstemplate'); ?>
-                <div class="col-md-4 no-print pull-right"> 
-                    <a data-toggle="modal" href="#myModal">
-                        <div class="btn-group pull-right">
-                            <button id="add_new" class="btn green btn-md">
-                                <i class="fa fa-plus-circle"></i> <?php echo lang('add_new'); ?> <?php echo lang('template'); ?>
-                            </button>
-                        </div>
-                    </a>
-                </div> 
-
-            </header>
-
-            <div class="panel-body">
-                <div class="adv-table editable-table ">
-
-                    <div class="space15"></div>
-                    <table class="table table-striped table-hover table-bordered" id="editable-sample1">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th><?php echo lang('templatename'); ?></th>                        
-                                <th><?php echo lang('options'); ?></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                        </tbody>
-                    </table>
+<div class="main-content">
+    <div class="section">
+        <div class="section-header">
+            <h1><?php echo lang('smstemplate'); ?></h1>
+        </div>
+        <?php
+        $message = $this->session->flashdata('feedback');
+        if (!empty($message)) {
+        ?><div class="alert alert-primary alert-has-icon alert-dismissible show fade">
+                <div class="alert-icon"><i class="far fa-lightbulb"></i></div>
+                <div class="alert-body">
+                    <button class="close" data-dismiss="alert">
+                        <span>&times;</span>
+                    </button>
+                    <div class="alert-title">Info!</div>
+                    <?= $message ?>
                 </div>
             </div>
-        </section>
-        <!-- page end-->
-    </section>
-</section>
+        <?php } ?>
+        <div class="section-body">
+            <div class="card">
+                <div class="card-header">
+                    <button class="btn btn-icon icon-left btn-primary" data-toggle="modal" data-target="#myModal"><i class="fas fa-plus"></i> <?php echo lang('add_new'); ?></button>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <div class="space15"></div>
+                        <table class="table table-striped table-bordered" id="editable-sample1">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th><?php echo lang('templatename'); ?></th>                        
+                                    <th><?php echo lang('options'); ?></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <!--main content end-->
 
 <!--  add Modal-->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-    <div class="modal-dialog">
+<div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h4 class="modal-title"><?php echo lang('add_new'); ?> <?php echo lang('manual'); ?> <?php echo lang('template');?></h4>
+                <h5 class="modal-title"><?php echo lang('add_new'); ?> <?php echo lang('manual'); ?> <?php echo lang('template');?></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-            <div class="modal-body">
-                <?php echo validation_errors(); ?>
-                <form role="form" name="myform1" action="sms/addNewManualTemplate" method="post" enctype="multipart/form-data">                                                                                    
-
+            <form role="form" name="myform1" action="sms/addNewManualTemplate" method="post" enctype="multipart/form-data">                                                                                    
+                <div class="modal-body">
+                    <?php echo validation_errors(); ?>
                     <div class="form-group">
                         <label for="exampleInputEmail1"> <?php echo lang('templatename'); ?></label>
                         <input type="text" class="form-control" name="name" id="exampleInputEmail1" value='' placeholder="" required>
@@ -79,26 +85,31 @@
                     </div>
                     <input type="hidden" name="id" value=''>
                     <input type="hidden" name="type" value='sms'>
-                    <button type="submit" name="submit" class="btn btn-info"><?php echo lang('submit'); ?></button>
-                </form>
-            </div>
+                    
+                    <div class="modal-footer bg-whitesmoke br">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary"><?php echo lang('submit'); ?></button>
+                    </div>
+                </div>
+            </form>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div>
 <!-- Add Area Modal-->
 
 <!-- Edit sms temp Modal-->
-<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-    <div class="modal-dialog">
+<div class="modal fade" id="myModal1" role="dialog">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h4 class="modal-title"><?php echo lang('edit'); ?> <?php echo lang('manual'); ?> <?php echo lang('template');?></h4>
+                <h5 class="modal-title"><?php echo lang('edit'); ?> <?php echo lang('manual'); ?> <?php echo lang('template');?></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-            <div class="modal-body">
-               <?php echo validation_errors(); ?>
-                <form role="form" id="smstemp" name="myform" action="sms/addNewManualTemplate" method="post" enctype="multipart/form-data">                                                                                    
-
+            <form role="form" name="myform" id="smstemp" action="sms/addNewManualTemplate" method="post" enctype="multipart/form-data">                                                                                    
+                <div class="modal-body">
+                    <?php echo validation_errors(); ?>
                     <div class="form-group">
                         <label for="exampleInputEmail1"> <?php echo lang('templatename'); ?></label>
                         <input type="text" class="form-control" name="name" id="exampleInputEmail1" value='' placeholder="" required>
@@ -106,26 +117,30 @@
                     <div class="form-group">
                         <label for="exampleInputEmail1"> <?php echo lang('message'); ?> <?php echo lang('template'); ?></label><br>
                         <?php
-                        $count1 = 0;
+                        $count = 0;
                         foreach ($shortcode as $shortcodes) {
                             ?>
-                            <input type="button" name="myBtn" value="<?php echo $shortcodes->name; ?>" onClick="addtext(this);">
+                            <input type="button" name="myBtn" value="<?php echo $shortcodes->name; ?>" onClick="addtext1(this);">
                             <?php
-                            $count1+=1;
-                            if ($count1 === 7) {
+                            $count+=1;
+                            if ($count === 7) {
                                 ?>
                                 <br>
                                 <?php
                             }
                         }
                         ?> <br><br>
-                         <textarea class="" name="message" id="editor4" value="" cols="70" rows="10"></textarea>
+                        <textarea class="" id="editor5" name="message" value='' cols="70" rows="10"></textarea>
                     </div>
                     <input type="hidden" name="id" value=''>
                     <input type="hidden" name="type" value='sms'>
-                    <button type="submit" name="submit" class="btn btn-info"><?php echo lang('submit'); ?></button>
-                </form>
-            </div>
+                    
+                    <div class="modal-footer bg-whitesmoke br">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary"><?php echo lang('submit'); ?></button>
+                    </div>
+                </div>
+            </form>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div>
