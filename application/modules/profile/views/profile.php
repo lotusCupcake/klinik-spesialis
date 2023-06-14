2ab19f264183bc98900b1b39cbc5f5a0b2566e82
lotusCupcake <fikriansari.mfa@gmail.com>
2023-06-14 15:37:55 +0700

    update: view profile/profile

--- application/modules/profile/views/profile.php
+++ application/modules/profile/views/profile.php
@@ -1,65 +1,87 @@
-<!--sidebar end-->
-<!--main content start-->
-<section id="main-content">
-    <section class="wrapper site-min-height">
-        <!-- page start-->
-        <div class="col-md-8 row">
-            <section class="panel">
-                <header class="panel-heading">
-                    <?php echo lang('manage_profile'); ?>
-                </header>
-                <div class="panel-body">
-                    <div class="adv-table editable-table ">
-                        <div class="clearfix">
-                            <?php echo validation_errors(); ?>
-                            <form role="form" action="profile/addNew" class="clearfix" method="post" enctype="multipart/form-data">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1"><?php echo lang('name'); ?></label>
-                                    <input type="text" class="form-control" name="name" id="exampleInputEmail1" value='<?php
-                                    if (!empty($profile->username)) {
-                                        echo $profile->username;
-                                    }
-                                    ?>' placeholder="">
+<!-- Main Content -->
+<div class="main-content no-print">
+    <section class="section">
+        <div class="section-header">
+            <h1><?php echo lang('manage_profile'); ?></h1>
+        </div>
+        <?php
+        $message = validation_errors();
+        if (!empty($message)) {
+        ?><div class="alert alert-danger alert-has-icon alert-dismissible show fade">
+                <div class="alert-icon"><i class="fas fa-exclamation-triangle"></i></div>
+                <div class="alert-body">
+                    <button class="close" data-dismiss="alert">
+                        <span>&times;</span>
+                    </button>
+                    <div class="alert-title">Failed!</div>
+                    <?= $message; ?>
+                </div>
+            </div>
+        <?php } ?>
+        <div class="section-body">
+            <div class="card">
+                <div class="card-body">
+                    <form role="form" action="profile/addNew" method="post" enctype="multipart/form-data">
+                        <div class="row" style="padding-right:30px">
+                            <div class="col-md-12 row mb-4">
+                                <div class="col-md-4 text-right">
+                                    <label class="col-form-label"><?php echo lang('name'); ?></label>
                                 </div>
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1"><?php echo lang('change_password'); ?></label>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control" name="name" id="exampleInputEmail1" value='<?php
+                                                                                                                        if (!empty($profile->username)) {
+                                                                                                                            echo $profile->username;
+                                                                                                                        }
+                                                                                                                        ?>' placeholder="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row" style="padding-right:30px">
+                            <div class="col-md-12 row mb-4">
+                                <div class="col-md-4 text-right">
+                                    <label class="col-form-label"><?php echo lang('change_password'); ?></label>
+                                </div>
+                                <div class="col-md-8">
                                     <input type="password" class="form-control" name="password" id="exampleInputEmail1" placeholder="********">
                                 </div>
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1"><?php echo lang('email'); ?></label>
-                                    <input type="text" class="form-control" name="email" id="exampleInputEmail1" value='<?php
-                                    if (!empty($profile->email)) {
-                                        echo $profile->email;
-                                    }
-                                    ?>' placeholder="" <?php
-                                           if (!empty($profile->username)) {
-                                               echo $profile->username;
-                                           }
-                                           ?>' placeholder="">
-                                </div>
-                                <input type="hidden" name="id" value='<?php
-                                if (!empty($profile->id)) {
-                                    echo $profile->id;
-                                }
-                                ?>'>
-                                <div class="form-group">
-                                    <button type="submit" name="submit" class="btn btn-info pull-right"><?php echo lang('submit'); ?></button>
-                                </div>
-                            </form>
+                            </div>
                         </div>
-                    </div>
+                        <div class="row" style="padding-right:30px">
+                            <div class="col-md-12 row mb-4">
+                                <div class="col-md-4 text-right">
+                                    <label class="col-form-label"><?php echo lang('email'); ?></label>
+                                </div>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control" name="email" id="exampleInputEmail1" value='<?php
+                                                                                                                        if (!empty($profile->email)) {
+                                                                                                                            echo $profile->email;
+                                                                                                                        }
+                                                                                                                        ?>' placeholder="">
+                                </div>
+                            </div>
+                        </div>
+                        <input type="hidden" name="id" value='<?php
+                                                                if (!empty($profile->id)) {
+                                                                    echo $profile->id;
+                                                                }
+                                                                ?>'>
+                        <div class="row" style="padding-right:30px">
+                            <div class="col-md-12 row mb-4">
+                                <div class="col-md-4 text-right">
+                                </div>
+                                <div class="col-md-8">
+                                    <button type="submit" name="submit" class="btn btn-primary"><?php echo lang('submit'); ?></button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </section>
-        </div>
-        <!-- page end-->
+            </div>
     </section>
-</section>
-<!--main content end-->
-<!--footer start-->
+</div>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
         $(".flashmessage").delay(3000).fadeOut(100);
     });
 </script>
