37b9019fce044f975bb0b0fd0776690d3350d3ee
lotusCupcake <fikriansari.mfa@gmail.com>
2023-06-15 10:33:34 +0700

    update: view donor/blood_bank

--- application/modules/donor/views/blood_bank.php
+++ application/modules/donor/views/blood_bank.php
@@ -1,144 +1,143 @@
-<!--sidebar end-->
-<!--main content start-->
-<section id="main-content">
-    <section class="wrapper site-min-height">
-        <!-- page start-->
-        <section class="panel">
-            <header class="panel-heading">
-                <?php echo lang('blood_bank'); ?>
-                <div class="col-md-4 pull-right">
-
-                    <div class="pull-right"></div>
-
-                </div>
-            </header>
-            <div class="panel-body">
-                <div class="adv-table editable-table ">
-                    <div class="clearfix">
+<!-- Main Content -->
+<div class="main-content">
+    <section class="section">
+        <div class="section-header">
+            <h1> <?php echo lang('blood_bank'); ?> </h1>
+        </div>
+        <div class="section-body">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <div class="space15"></div>
+                        <table class="table table-striped table-bordered" id="editable-sample">
+                            <thead>
+                                <tr>
+                                    <th>Blood Group</th>
+                                    <th>Status</th>
+                                    <th>Options</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($groups as $group) { ?>
+                                    <tr class="">
+                                        <td><?php echo $group->group; ?></td>
+                                        <td> <?php echo $group->status; ?></td>
+                                        <td>
+                                            <button type="button" class="btn btn-info btn-xs btn_width editbutton" title="<?php echo lang('edit'); ?>" data-toggle="modal" data-id="<?php echo $group->id; ?>"><i class="fa fa-edit"></i> </button>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
                     </div>
-                    <button class="export" onclick="javascript:window.print();"><?php echo lang('print'); ?></button>  
-                    <div class="space15"></div>
-                    <table class="table table-striped table-hover table-bordered" id="editable-sample">
-                        <thead>
-                            <tr>
-                                <th>Blood Group</th>
-                                <th>Status</th>
-                                <th>Options</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <style>
-                            .img_url{
-                                height:20px;
-                                width:20px;
-                                background-size: contain; 
-                                max-height:20px;
-                                border-radius: 100px;
-                            }
-
-                        </style>
-                        <?php foreach ($groups as $group) { ?>
-                            <tr class="">
-                                <td><?php echo $group->group; ?></td>
-                                <td> <?php echo $group->status; ?></td>
-                                <td>
-                                    <button type="button" class="btn btn-info btn-xs btn_width editbutton" title="<?php echo lang('edit'); ?>" data-toggle="modal" data-id="<?php echo $group->id; ?>"><i class="fa fa-edit"></i> </button>   
-                                </td>
-                            </tr>
-                        <?php } ?>
-                        </tbody>
-                    </table>
                 </div>
             </div>
-        </section>
-        <!-- page end-->
+        </div>
     </section>
-</section>
-<!--main content end-->
-<!--footer start-->
+</div>
 
-
-
-
-
-
-
-
-<!-- Edit Event Modal-->
-<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-    <div class="modal-dialog">
+<div class="modal fade" role="dialog" id="myModal2">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h4 class="modal-title">  <?php echo lang('update_blood_bank'); ?></h4>
+                <h5 class="modal-title"><?php echo lang('update_blood_bank'); ?></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-            <div class="modal-body">
-                <form role="form" id="editBloodBank" action="donor/updateBloodBank" method="post" enctype="multipart/form-data">
-                    <div class="form-group"> 
+            <form id="editBloodBank" action="donor/updateBloodBank" class="clearfix" method="post" enctype="multipart/form-data">
+                <div class="modal-body">
+                    <div class="form-group">
                         <label for="exampleInputEmail1"><?php echo lang('group'); ?></label>
-                        <input type="text" class="form-control" name="group" id="exampleInputEmail1" value='' placeholder="" disabled>    
+                        <input type="text" class="form-control" name="group" id="exampleInputEmail1" value='' placeholder="" readonly>
                     </div>
                     <div class="form-group">
                         <label for="exampleInputEmail1"><?php echo lang('status'); ?></label>
                         <input type="text" class="form-control" name="status" id="exampleInputEmail1" value='' placeholder="">
                     </div>
                     <input type="hidden" name="id" value=''>
-                    <button type="submit" name="submit" class="btn btn-info"><?php echo lang('submit'); ?></button>
-                </form>
-
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
+                </div>
+                <div class="modal-footer bg-whitesmoke br">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary"><?php echo lang('submit'); ?></button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
-<!-- Edit Event Modal-->
 
 <script src="common/js/codearistos.min.js"></script>
 <script type="text/javascript">
-                        $(document).ready(function () {
-                            $(".editbutton").click(function (e) {
-                                e.preventDefault(e);
-                                // Get the record's ID via attribute  
-                                var iid = $(this).attr('data-id');
-                                $('#editBloodBank').trigger("reset");
-                                $('#myModal2').modal('show');
-                                $.ajax({
-                                    url: 'donor/updateBloodBankByJason?id=' + iid,
-                                    method: 'GET',
-                                    data: '',
-                                    dataType: 'json',
-                                }).success(function (response) {
-                                    // Populate the form fields with the data returned from server
-                                    $('#editBloodBank').find('[name="id"]').val(response.bloodbank.id).end()
-                                    $('#editBloodBank').find('[name="group"]').val(response.bloodbank.group).end()
-                                    $('#editBloodBank').find('[name="status"]').val(response.bloodbank.status).end()
+    $(document).ready(function() {
+        $(".editbutton").click(function(e) {
+            e.preventDefault(e);
+            // Get the record's ID via attribute  
+            var iid = $(this).attr('data-id');
+            $('#editBloodBank').trigger("reset");
+            $('#myModal2').modal('show');
+            $.ajax({
+                url: 'donor/updateBloodBankByJason?id=' + iid,
+                method: 'GET',
+                data: '',
+                dataType: 'json',
+            }).success(function(response) {
+                $('#editBloodBank').find('[name="id"]').val(response.bloodbank.id).end()
+                $('#editBloodBank').find('[name="group"]').val(response.bloodbank.group).end()
+                $('#editBloodBank').find('[name="status"]').val(response.bloodbank.status).end()
 
-                                });
-                            });
-                        });
+            });
+        });
+    });
 </script>
 
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
         var table = $('#editable-sample').DataTable({
             responsive: true,
 
             dom: "<'row'<'col-sm-3'l><'col-sm-5 text-center'B><'col-sm-4'f>>" +
-                    "<'row'<'col-sm-12'tr>>" +
-                    "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+                "<'row'<'col-sm-12'tr>>" +
+                "<'row'<'col-sm-5'i><'col-sm-7'p>>",
 
-            buttons: [
-                {extend: 'copyHtml5', exportOptions: {columns: [0, 1], }},
-                {extend: 'excelHtml5', exportOptions: {columns: [0, 1], }},
-                {extend: 'csvHtml5', exportOptions: {columns: [0, 1], }},
-                {extend: 'pdfHtml5', exportOptions: {columns: [0, 1], }},
-                {extend: 'print', exportOptions: {columns: [0, 1], }},
+            buttons: [{
+                    extend: 'copyHtml5',
+                    exportOptions: {
+                        columns: [0, 1],
+                    }
+                },
+                {
+                    extend: 'excelHtml5',
+                    exportOptions: {
+                        columns: [0, 1],
+                    }
+                },
+                {
+                    extend: 'csvHtml5',
+                    exportOptions: {
+                        columns: [0, 1],
+                    }
+                },
+                {
+                    extend: 'pdfHtml5',
+                    exportOptions: {
+                        columns: [0, 1],
+                    }
+                },
+                {
+                    extend: 'print',
+                    exportOptions: {
+                        columns: [0, 1],
+                    }
+                },
             ],
             aLengthMenu: [
                 [10, 25, 50, 100, -1],
                 [10, 25, 50, 100, "All"]
             ],
             iDisplayLength: -1,
-            "order": [[0, "desc"]],
+            "order": [
+                [0, "desc"]
+            ],
 
             "language": {
                 "lengthMenu": "_MENU_",
@@ -151,8 +150,7 @@
 </script>
 
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
         $(".flashmessage").delay(3000).fadeOut(100);
     });
 </script>
-
