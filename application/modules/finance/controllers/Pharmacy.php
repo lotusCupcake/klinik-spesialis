92f84cc142063d8884edd14fb1f66ea26efa5a76
lotusCupcake <fikriansari.mfa@gmail.com>
2023-08-08 10:22:36 +0700

    fix: pharmacist

--- application/modules/finance/controllers/Pharmacy.php
+++ application/modules/finance/controllers/Pharmacy.php
@@ -13,7 +13,7 @@ class Pharmacy extends MX_Controller
         $this->load->model('medicine/medicine_model');
         $this->load->model('settings/settings_model');
         $data['settings'] = $this->settings_model->getSettings();
-        if (!$this->ion_auth->in_group(array('admin', 'Accountant', 'Receptionist'))) {
+        if (!$this->ion_auth->in_group(array('admin', 'Accountant', 'Receptionist', "Pharmacist"))) {
             redirect('home/permission');
         }
     }
